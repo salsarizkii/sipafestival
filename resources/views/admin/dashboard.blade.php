ec4526b48f9754e4df8d8b35083499fa9c74fb88
salsarizkii <salsarizki.srs@gmail.com>
2025-05-06 23:55:21 +0700

    enhance complaint response display and add reply link

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -33,19 +33,38 @@
                               <p class="my-0 text-muted" style="font-size: 0.75rem;">
                                 {{ $complaint->created_at->format('H.i') }}
                             </p>
+                            @if ($complaint->status === 'belum dibalas')
                               <a href="{{ route('admin.dashboard.sendEmail', $complaint->id) }}"
                                   class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover text-secondary"
                                   style="font-size: 0.75rem;"> balas</a>
+                            @endif
                                   
-                                
-
-                                @if ($complaint->status === 'sudah dibalas')
-                                    <p class="my-0 text-muted" style="font-size: 0.75rem;">{{ $complaint->response_subject }}</p>
-                                    <p class="my-0 text-muted" style="font-size: 0.75rem;">{{ $complaint->response_message }}</p>
-                                @endif
+                            @if ($complaint->status === 'sudah dibalas')
+                            <div class="d-flex mt-2 ms-3 align-items-start">
+                                <!-- Garis vertikal tipis -->
+                                <hr><hr><hr>
+                                <div class="me-2">
+                                    <i class="bi bi-reply-fill"></i>
+                                </div>
+                            
+                                <!-- Konten balasan -->
+                                <div>
+                                    <p class="my-0">{{ $complaint->response_subject }}</p>
+                                    <p class="my-0">{{ $complaint->response_message }}</p>
+                                </div>
+                            </div>
+                            @endif
+                            
                                 
                           </td>
-                          <td>{{ $complaint->status }}</td>
+                          <td class="align-middle" style="white-space: nowrap;">
+                            @if ($complaint->status === 'sudah dibalas')
+                                <span class="badge bg-success">sudah dibalas</span>
+                            @else
+                                <span class="badge bg-secondary">belum dibalas</span>
+                            @endif
+                        </td>
+
                       </tr>
                   @endforeach
               </tbody>
